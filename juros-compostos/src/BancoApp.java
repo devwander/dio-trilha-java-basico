2e7c0e51b3e93e6a4d6b68613a49087de78e225e
devwander <josewanderson173@gmail.com>
2023-09-24 22:29:28 -0300

    feat: Java Challenges: Compound Interest

---
+++ juros-compostos/src/BancoApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BancoApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double valorInicial = scanner.nextDouble();
+        double taxaJuros = scanner.nextDouble();
+        int periodo = scanner.nextInt();
+        scanner.close();
+
+        double valorFinal = valorInicial * Math.pow(1 + taxaJuros, periodo);
+        System.out.println("Valor final do investimento: R$ " + String.format("%.2f", valorFinal));
+    }
+}
