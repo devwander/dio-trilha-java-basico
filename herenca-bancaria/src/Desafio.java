1bbb41be6987c340ea3e56e7c377599c51310041
devwander <josewanderson173@gmail.com>
2023-10-15 19:09:18 -0300

    Java Challenges: Bank Inheritance

---
+++ herenca-bancaria/src/Desafio.java
@@ -0,0 +1,51 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Desafio {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String titular = scanner.next();
+        int numeroConta = scanner.nextInt();
+        double saldo = scanner.nextDouble();
+        double taxaJuros = scanner.nextDouble();
+        scanner.close();
+
+        ContaPoupanca contaPoupanca = new ContaPoupanca(numeroConta, titular, saldo, taxaJuros);
+
+        System.out.println("Conta Poupanca:");
+        contaPoupanca.exibirInformacoes();
+    }
+}
+
+class ContaBancaria {
+    protected int numero;
+    protected String titular;
+    protected double saldo;
+    protected double taxaJuros;
+
+    public ContaBancaria(int numero, String titular, double saldo, double taxaJuros) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+        this.taxaJuros = taxaJuros;
+    }
+
+    public void exibirInformacoes() {
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        System.out.println(titular);
+        System.out.println(numero);
+        System.out.println("Saldo: R$ " + decimalFormat.format(saldo));
+        System.out.println("Taxa de juros: " + taxaJuros + "%");
+    }
+}
+
+class ContaPoupanca extends ContaBancaria {
+    public ContaPoupanca(int numero, String titular, double saldo, double taxaJuros) {
+        super(numero, titular, saldo, taxaJuros);
+    }
+
+    public void exibirInformacoes() {
+        super.exibirInformacoes();
+    }
+}
