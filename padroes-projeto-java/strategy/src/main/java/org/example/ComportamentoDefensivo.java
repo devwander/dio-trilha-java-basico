6e188b51c9d9f866478359a6c106ef731ae7e3ba
devwander <josewanderson173@gmail.com>
2023-10-22 21:27:15 -0300

    feat: strategy

---
+++ padroes-projeto-java/strategy/src/main/java/org/example/ComportamentoDefensivo.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class ComportamentoDefensivo implements Comportamento{
+
+    @Override
+    public void mover() {
+        System.out.println("MOVENDO-SE EM DEFESA");
+    }
+}
---
+++ padroes-projeto-java/strategy/src/main/java/org/example/ComportamentoNormal.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class ComportamentoNormal implements Comportamento{
+
+    @Override
+    public void mover() {
+        System.out.println("MOVENDO-SE NORMALMENTE");
+    }
+}
